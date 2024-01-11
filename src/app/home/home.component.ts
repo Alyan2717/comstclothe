bb204b592eba04bccc068483941b867a832cced5
AlyanKhan <alyan.khan@decibelbpo.com>
2024-01-11 10:38:13 +0500

    modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-//||==========================|| Product Slider ||==========================||
+  slickModal: any;
+  public show: any
+  //==========================|| Product Slider ||==========================//
   slides = [
     { img: './assets/pr_1.jpg', name: 'Test1' },
     { img: './assets/pr_2.jpg', name: 'Test2' },
@@ -36,4 +34,24 @@ export class HomeComponent implements OnInit {
     speed: 1000, // Transition speed in milliseconds
     autoplaySpeed: 2000 // Autoplay interval in milliseconds
   };
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    // Subscribe to the viewport size changes
+    this.breakpointObserver.observe(['(max-width: 767px)', '(min-width: 768px) and (max-width: 991px)'])
+      .subscribe(result => {
+        if (result.breakpoints['(max-width: 767px)']) {
+          // For mobile screens
+          this.slideConfig.slidesToShow = 1;
+        } else if (result.breakpoints['(max-width: 768px) and (max-width: 1024px)']) {
+          // For tablets
+          this.slideConfig.slidesToShow = 3;
+        } else {
+          // For larger screens (if needed)
+          this.slideConfig.slidesToShow = 4;
+        }
+      });
+  }
+
+  ngOnInit(): void {
+  }
 }
