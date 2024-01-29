79396c3b157350ad4e063e040af1fa0b89a049d9
AlyanKhan <alyan.khan@decibelbpo.com>
2024-01-29 10:03:04 +0500

    shop modified

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -31,12 +31,20 @@ export class ShopComponent implements OnInit {
       { id: '1', image: './assets/pr_1.jpg', name: 'Item 1', description: 'Description 1' },
       { id: '2', image: './assets/pr_2.jpg', name: 'Item 2', description: 'Description 2' },
       { id: '3', image: './assets/pr_3.jpg', name: 'Item 3', description: 'Description 3' },
-      { id: '4', image: './assets/pr_4.jpg', name: 'Item 3', description: 'Description 3' },
-      { id: '5', image: './assets/pr_5.jpg', name: 'Item 3', description: 'Description 3' },
-      { id: '6', image: './assets/pr_6.jpg', name: 'Item 3', description: 'Description 3' },
-      { id: '7', image: './assets/pr_7.jpg', name: 'Item 3', description: 'Description 3' },
-      { id: '8', image: './assets/pr_8.jpg', name: 'Item 3', description: 'Description 3' },
+      { id: '4', image: './assets/pr_4.jpg', name: 'Item 4', description: 'Description 4' },
+      { id: '5', image: './assets/pr_5.jpg', name: 'Item 5', description: 'Description 5' },
+      { id: '6', image: './assets/pr_6.jpg', name: 'Item 6', description: 'Description 6' },
+      { id: '7', image: './assets/pr_7.jpg', name: 'Item 7', description: 'Description 7' },
+      { id: '8', image: './assets/pr_8.jpg', name: 'Item 8', description: 'Description 8' },
     ];
+  }
+
+  onBuy(data: any) {
+    console.log("🚀 ~ ShopComponent ~ onBuy ~ data:", data)
+  }
+
+  onAddtoCart(data: any) {
+    console.log("🚀 ~ ShopComponent ~ onAddtoCart ~ data:", data)
   }
 
 }
