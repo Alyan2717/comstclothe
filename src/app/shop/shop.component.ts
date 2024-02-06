253f72687fb6f47265d051880fbe01b5e4a45f25
AlyanKhan <alyan.khan@decibelbpo.com>
2024-02-06 14:40:21 +0500

    worked on shop page

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -10,34 +10,40 @@ import { ActivatedRoute } from '@angular/router';
 export class ShopComponent implements OnInit {
   //Variables
   searchText: string = '';
-  selectedId: string = '';
-  shopItems: any[] = [];
+  selectedCategory: string = '';
+  // shopItems: any[] = [];
+  shopItems: any[] = [
+    { id: '1', image: './assets/pr_1.jpg', name: 'Item 1', description: 'Description 1', category: 'tshirt' },
+    { id: '2', image: './assets/pr_2.jpg', name: 'Item 2', description: 'Description 2', category: 'tshirt' },
+    { id: '3', image: './assets/pr_3.jpg', name: 'Item 3', description: 'Description 3', category: 'sweatshirt' },
+    { id: '4', image: './assets/pr_4.jpg', name: 'Item 4', description: 'Description 4', category: 'sweatshirt' },
+    { id: '5', image: './assets/pr_5.jpg', name: 'Item 5', description: 'Description 5', category: 'sweatshirt' },
+    { id: '6', image: './assets/pr_6.jpg', name: 'Item 6', description: 'Description 6', category: 'hoody' },
+    { id: '7', image: './assets/pr_7.jpg', name: 'Item 7', description: 'Description 7', category: 'hoody' },
+    { id: '8', image: './assets/pr_8.jpg', name: 'Item 8', description: 'Description 8', category: 'hoody' },
+  ];
+  filterShopItems: any[] = [];
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-
     // fetch id of category for filter
     this.route.queryParams.subscribe(params => {
-      this.selectedId = params['id'];
-      console.log("🚀 ~ ShopComponent ~ ngOnInit ~ this.selectedId:", this.selectedId)
+      this.selectedCategory = params['id'];
+      this.getProducts();
     });
-
-    this.getProducts();
   }
 
   // fetch Products from API
   getProducts() {
-    this.shopItems = [
-      { id: '1', image: './assets/pr_1.jpg', name: 'Item 1', description: 'Description 1' },
-      { id: '2', image: './assets/pr_2.jpg', name: 'Item 2', description: 'Description 2' },
-      { id: '3', image: './assets/pr_3.jpg', name: 'Item 3', description: 'Description 3' },
-      { id: '4', image: './assets/pr_4.jpg', name: 'Item 4', description: 'Description 4' },
-      { id: '5', image: './assets/pr_5.jpg', name: 'Item 5', description: 'Description 5' },
-      { id: '6', image: './assets/pr_6.jpg', name: 'Item 6', description: 'Description 6' },
-      { id: '7', image: './assets/pr_7.jpg', name: 'Item 7', description: 'Description 7' },
-      { id: '8', image: './assets/pr_8.jpg', name: 'Item 8', description: 'Description 8' },
-    ];
+    this.applyFilter();
+  }
+
+  applyFilter() {
+    //-- For filter by category
+    if (this.selectedCategory !== "all") {
+      this.shopItems.filter(f => f.category == this.selectedCategory);
+    }
   }
 
   onBuy(data: any) {
